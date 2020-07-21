94d5768f0ebf4d69ce61f9d256e8b7918dfe1a0a
Jay <beginin15@gmail.com>
2020-07-21 21:21:31 +0900

    [ioc] xml 파일에서 컴포넌트 스캔을 위한 베이스 패키지 설정하는 방법
    - 컴포넌트 스캔을 수행할 패키지를 지정한다. 해당 패키지 아래에서 @Component 어노테이션이 붙은 클래스를 찾아 빈으로 등록한다.
    
    - 빈으로 등록할 클래스에 Component 어노테이션을 붙인다. (BookService, BookRepository)

--- src/main/java/com/course/spring_framework/core/BookRepository.java
+++ src/main/java/com/course/spring_framework/core/BookRepository.java
@@ -1,4 +1,7 @@
 package com.course.spring_framework.core;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
--- src/main/java/com/course/spring_framework/core/BookService.java
+++ src/main/java/com/course/spring_framework/core/BookService.java
@@ -1,7 +1,12 @@
 package com.course.spring_framework.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
+    @Autowired
     private BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
