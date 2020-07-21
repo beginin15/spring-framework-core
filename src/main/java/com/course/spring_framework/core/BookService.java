8441de6bfe1f9552b847842a10ba48200d7c17e6
Jay <beginin15@gmail.com>
2020-07-21 21:35:24 +0900

    [ioc] xml 대신 자바 설정 파일 정의, AnnotationConfigApplicationContext로 빈 가져오기

--- src/main/java/com/course/spring_framework/core/BookService.java
+++ src/main/java/com/course/spring_framework/core/BookService.java
@@ -1,12 +1,10 @@
 package com.course.spring_framework.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
-    @Autowired
     private BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -1,14 +1,14 @@
 package com.course.spring_framework.core;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class SpringFrameworkCoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
 
         BookService bookService = (BookService) context.getBean("bookService");
