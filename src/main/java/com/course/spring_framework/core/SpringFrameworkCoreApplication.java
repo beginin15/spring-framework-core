4f1b9e504ee8fa60bcf42927158c6713a8cc6011
Jay <beginin15@gmail.com>
2020-07-21 21:52:20 +0900

    [ioc] @SpringBootApplication 어노테이션을 이용하여 자바 설정 파일 없이 어노테이션만으로 빈 등록하기

--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -1,18 +1,12 @@
 package com.course.spring_framework.core;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
+@SpringBootApplication
 public class SpringFrameworkCoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
 
-        BookService bookService = (BookService) context.getBean("bookService");
-        System.out.println(bookService.getBookRepository() != null);
     }
 
 }
