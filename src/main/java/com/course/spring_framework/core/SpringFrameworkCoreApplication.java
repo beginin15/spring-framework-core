1d445f057fc202831117d5e65236b0be01e1e367
Jay <beginin15@gmail.com>
2020-07-21 16:04:20 +0900

    project init

---
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -0,0 +1,13 @@
+package com.course.spring_framework.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/spring_framework/core/SpringFrameworkCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.spring_framework.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
