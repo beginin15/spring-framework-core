9151a6b81f8fbabdc5dcefa8dd537e8ecb5dcbdd
Jay <beginin15@gmail.com>
2020-07-21 21:44:51 +0900

    [ioc] @ComponentScan을 이용하여 빈을 직접 정의하지 않고 어노테이션 기반으로 빈을 생성

--- src/main/java/com/course/spring_framework/core/ApplicationConfig.java
+++ src/main/java/com/course/spring_framework/core/ApplicationConfig.java
@@ -1,18 +1,10 @@
 package com.course.spring_framework.core;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses = SpringFrameworkCoreApplication.class)
 public class ApplicationConfig {
 
-    @Bean
-    public BookRepository bookRepository() {
-        return new BookRepository();
-    }
-
-    @Bean
-    public BookService bookService() {
-        return new BookService();
-    }
 }
