38a7d0fe2e89c74cb2598397e3b7ee69827d77ed
Jay <beginin15@gmail.com>
2020-07-22 14:41:27 +0900

    [ioc] 의존 객체를 리스트로 모두 받을 수 있는 것 확인

--- src/main/java/com/course/spring_framework/core/BookService.java
+++ src/main/java/com/course/spring_framework/core/BookService.java
@@ -1,16 +1,17 @@
 package com.course.spring_framework.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
 
-    @Autowired @Qualifier(value = "yourBookRepository")
-    private BookRepository bookRepository;
+    @Autowired
+    private List<BookRepository> bookRepositories;
 
     public void pringBookRepository() {
-        System.out.println(bookRepository);
+        bookRepositories.forEach(System.out::println);
     }
 }
