33f87f0e02c7048d7dab7d5dc9fc563eef7bbb76
Jay <beginin15@gmail.com>
2020-07-22 15:49:07 +0900

    [ioc] InitializingBean의 메소드 대신 `@PostContruct`를 사용하여 초기화 메소드 작성

--- src/main/java/com/course/spring_framework/core/BookService.java
+++ src/main/java/com/course/spring_framework/core/BookService.java
@@ -1,17 +1,18 @@
 package com.course.spring_framework.core;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
-public class BookService implements InitializingBean {
+public class BookService {
 
     @Autowired
     private BookRepository myBookRepository;
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void setUp() {
         System.out.println(myBookRepository);
     }
 }
