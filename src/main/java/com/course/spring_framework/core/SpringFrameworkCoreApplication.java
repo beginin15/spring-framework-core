d079da75480ccaac187a715c3c00e26ce7f22bc9
Jay <beginin15@gmail.com>
2020-07-23 21:32:13 +0900

    [ioc] @ComponentScan에 포함되지 않는 패키지의 컴포넌트를 스캔하지 못하는 것 확인

--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -1,10 +1,15 @@
 package com.course.spring_framework.core;
 
+import com.course.spring_framework.out.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringFrameworkCoreApplication {
+
+    @Autowired
+    private TestService testService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFrameworkCoreApplication.class, args);
