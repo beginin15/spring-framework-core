6b13395b5adcd6941414be02a0339043a4dc93f3
Jay <beginin15@gmail.com>
2020-07-24 14:55:57 +0900

    [ioc] Functional을 이용한 ApplicationRunner 등록

--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -2,11 +2,14 @@ package com.course.spring_framework.core;
 
 import com.course.spring_framework.out.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class SpringFrameworkCoreApplication {
@@ -18,6 +21,17 @@ public class SpringFrameworkCoreApplication {
         SpringApplication app = new SpringApplication(SpringFrameworkCoreApplication.class);
         app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) context -> {
             context.registerBean(TestService.class);
+            context.registerBean(ApplicationRunner.class, new Supplier<ApplicationRunner>() {
+                @Override
+                public ApplicationRunner get() {
+                    return new ApplicationRunner() {
+                        @Override
+                        public void run(ApplicationArguments args) throws Exception {
+                            System.out.println("Application Runner!");
+                        }
+                    };
+                }
+            });
         });
         app.run(args);
     }
