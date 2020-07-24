75816c7af1e2b2ed7a1d7761e650da3377e2b124
Jay <beginin15@gmail.com>
2020-07-24 15:21:00 +0900

    [ioc] 기본적으로 빈의 스코프는 싱글톤 확인

---
+++ src/main/java/com/course/spring_framework/core/Proto.java
@@ -0,0 +1,7 @@
+package com.course.spring_framework.core;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Proto {
+}
---
+++ src/main/java/com/course/spring_framework/core/TestRunner.java
@@ -0,0 +1,22 @@
+package com.course.spring_framework.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements ApplicationRunner {
+
+    @Autowired
+    private Single single;
+
+    @Autowired
+    private Proto proto;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(proto);
+        System.out.println(single.getProto());
+    }
+}
