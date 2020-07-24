f3d8107e6f8ebd1d8636a662bbe053dab4ec7f5e
Jay <beginin15@gmail.com>
2020-07-24 16:16:11 +0900

    [ioc] ObjectProvider를 이용하여 프로토타입 스코프 구현

--- src/main/java/com/course/spring_framework/core/Proto.java
+++ src/main/java/com/course/spring_framework/core/Proto.java
@@ -1,9 +1,8 @@
 package com.course.spring_framework.core;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Component @Scope(value = "prototype")
 public class Proto {
 }
