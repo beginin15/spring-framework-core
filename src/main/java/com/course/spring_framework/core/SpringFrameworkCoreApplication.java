cbe796243d6a7c98c51d4603ece0b90da82bdf5a
Jay <beginin15@gmail.com>
2020-07-24 14:57:56 +0900

    [ioc] 람다 표현식으로 대체

--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -2,14 +2,11 @@ package com.course.spring_framework.core;
 
 import com.course.spring_framework.out.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.support.GenericApplicationContext;
-
-import java.util.function.Supplier;
 
 @SpringBootApplication
 public class SpringFrameworkCoreApplication {
@@ -21,17 +18,7 @@ public class SpringFrameworkCoreApplication {
         SpringApplication app = new SpringApplication(SpringFrameworkCoreApplication.class);
         app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) context -> {
             context.registerBean(TestService.class);
-            context.registerBean(ApplicationRunner.class, new Supplier<ApplicationRunner>() {
-                @Override
-                public ApplicationRunner get() {
-                    return new ApplicationRunner() {
-                        @Override
-                        public void run(ApplicationArguments args) throws Exception {
-                            System.out.println("Application Runner!");
-                        }
-                    };
-                }
-            });
+            context.registerBean(ApplicationRunner.class, () -> args1 -> System.out.println("Application Runner!"));
         });
         app.run(args);
     }
