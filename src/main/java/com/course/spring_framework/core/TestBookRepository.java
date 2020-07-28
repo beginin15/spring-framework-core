00a20cd8519c44b403a566e986528a975ca4e1ce
Jay <beginin15@gmail.com>
2020-07-29 01:24:55 +0900

    [ioc] 빈(컴포넌트) 클래스에 직접 Profile 적용

--- src/main/java/com/course/spring_framework/core/TestBookRepository.java
+++ src/main/java/com/course/spring_framework/core/TestBookRepository.java
@@ -1,4 +1,9 @@
 package com.course.spring_framework.core;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("test")
 public class TestBookRepository extends BookRepository {
 }
