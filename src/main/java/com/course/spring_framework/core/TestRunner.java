7174a12c54f15c93ee5bfdab3da5ca6b91f5095b
Jay <beginin15@gmail.com>
2020-07-29 01:57:39 +0900

    [ioc] 스프링부트에서 제공하는 '@Value'를 사용하여 프로퍼티 가져오기

--- src/main/java/com/course/spring_framework/core/TestRunner.java
+++ src/main/java/com/course/spring_framework/core/TestRunner.java
@@ -1,6 +1,7 @@
 package com.course.spring_framework.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
@@ -13,9 +14,13 @@ public class TestRunner implements ApplicationRunner {
     @Autowired
     ApplicationContext context;
 
+    @Value("${app.user}")
+    String user;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = context.getEnvironment();
         System.out.println(environment.getProperty("app.name"));
+        System.out.println(user);
     }
 }
