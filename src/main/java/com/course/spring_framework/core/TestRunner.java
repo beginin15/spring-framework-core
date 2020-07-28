f0ec1f54a00c5567fc7894164dab16e9226d58f0
Jay <beginin15@gmail.com>
2020-07-29 02:21:16 +0900

    [ioc] MessageSource 사용하기

---
+++ src/main/java/com/course/spring_framework/core/TestRunner.java
@@ -0,0 +1,22 @@
+package com.course.spring_framework.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class TestRunner implements ApplicationRunner {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(messageSource.getMessage("greeting", new String[] {"jay"}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greeting", new String[] {"jay"}, Locale.getDefault()));
+    }
+}
