59af5537a01cfe0e234ec4e914917f36050876d2
Jay <beginin15@gmail.com>
2020-07-29 01:54:41 +0900

    [ioc] JVM 옵션 vs 프로퍼티 소스 우선순위 대결
    - 프로퍼티는 계층형이므로 우선순위가 존재한다.
    - 동일한 이름의 프로퍼티가 존재하면 프로퍼티 소스보다 JVM 옵션이 우선이었다.

--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -2,8 +2,10 @@ package com.course.spring_framework.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class SpringFrameworkCoreApplication {
 
     public static void main(String[] args) {
