9ac278bc160573e118ee035e352dfc7d698089b2
Jay <beginin15@gmail.com>
2020-07-29 01:48:14 +0900

    [ioc] JVM 옵션으로 프로퍼티 값 설정하기

---
+++ src/main/java/com/course/spring_framework/core/TestRunner.java
@@ -0,0 +1,21 @@
+package com.course.spring_framework.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements ApplicationRunner {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Environment environment = context.getEnvironment();
+        System.out.println(environment.getProperty("app.name"));
+    }
+}
