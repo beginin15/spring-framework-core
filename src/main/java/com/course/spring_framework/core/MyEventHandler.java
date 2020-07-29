be4778a4d0b15e87a8faa0308dcecc3ed2ee0e74
Jay <beginin15@gmail.com>
2020-07-29 17:19:08 +0900

    [ioc] Ordered 어노테이션으로 우선순위 지정

--- src/main/java/com/course/spring_framework/core/MyEventHandler.java
+++ src/main/java/com/course/spring_framework/core/MyEventHandler.java
@@ -1,12 +1,15 @@
 package com.course.spring_framework.core;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받았다! " + event.getData());
