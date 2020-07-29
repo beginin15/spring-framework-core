0912ac75f35a42d9375e4397269040f71fb57086
Jay <beginin15@gmail.com>
2020-07-29 17:22:01 +0900

    [ioc] 상수값을 더하고 빼서 우선순위의 앞뒤 지정 가능

--- src/main/java/com/course/spring_framework/core/AnotherHandler.java
+++ src/main/java/com/course/spring_framework/core/AnotherHandler.java
@@ -1,12 +1,15 @@
 package com.course.spring_framework.core;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherHandler {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE + 2)
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("또 다른 핸들러, " + event.getData());
