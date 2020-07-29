c60eb35e1bd90ab6e3a0544366d83dda4a9cbdb4
Jay <beginin15@gmail.com>
2020-07-29 17:13:09 +0900

    [ioc] 동일한 이벤트 객체를 처리하는 핸들러가 2개 이상일 때
    - 별다른 설정을 하지 않으면 동일한 스레드가 순차적으로 이벤트를 처리한다.

--- src/main/java/com/course/spring_framework/core/AnotherHandler.java
+++ src/main/java/com/course/spring_framework/core/AnotherHandler.java
@@ -4,11 +4,11 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyEventHandler {
+public class AnotherHandler {
 
     @EventListener
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
-        System.out.println("이벤트 받았다! " + event.getData());
+        System.out.println("또 다른 핸들러, " + event.getData());
     }
 }
