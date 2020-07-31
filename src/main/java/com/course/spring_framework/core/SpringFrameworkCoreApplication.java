69309144cec9f402e5f020a79f38ae7a790c9cbd
Jay <beginin15@gmail.com>
2020-07-31 14:07:22 +0900

    [ioc] '@Async'를 이용하여 비동기적으로 이벤트 처리

--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -2,8 +2,10 @@ package com.course.spring_framework.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringFrameworkCoreApplication {
 
     public static void main(String[] args) {
