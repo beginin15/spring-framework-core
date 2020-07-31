6ba7d8b81f39800da9d938ee3172b1d6d0168fae
Jay <beginin15@gmail.com>
2020-07-31 14:18:02 +0900

    [ioc] 스프링에서 제공하는 기본 이벤트

--- src/main/java/com/course/spring_framework/core/MyEventHandler.java
+++ src/main/java/com/course/spring_framework/core/MyEventHandler.java
@@ -1,5 +1,7 @@
 package com.course.spring_framework.core;
 
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -12,5 +14,17 @@ public class MyEventHandler {
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받았다! " + event.getData());
+    }
+
+    @EventListener
+    public void handle(ContextRefreshedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    public void handle(ContextClosedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
     }
 }
