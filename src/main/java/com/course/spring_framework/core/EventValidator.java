e6185b2b24fd145c20a564f66ef8c37d491da680
Jay <beginin15@gmail.com>
2020-08-20 19:02:23 +0900

    [abstraction] ValidationUtils를 사용하지 않고 직접 검증 로직을 작성
    - errors.reject() : 객체 전반에 대한 검증 실패 시
    - errors.rejectValue() : 객체의 특정 프로퍼티에 대한 검증 실패 시

--- src/main/java/com/course/spring_framework/core/EventValidator.java
+++ src/main/java/com/course/spring_framework/core/EventValidator.java
@@ -1,7 +1,6 @@
 package com.course.spring_framework.core;
 
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class EventValidator implements Validator {
@@ -13,7 +12,9 @@ public class EventValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        // defaultMessage는 errorCode로 메세지를 찾지 못했을 때 사용되는 기본 메세지
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "notempty", "Empty is now allowed");
+        Event event = (Event) target;
+        if (event.getTitle() == null) {
+            errors.rejectValue("title", "notempty", "Empty is now allowed");
+        }
     }
 }
