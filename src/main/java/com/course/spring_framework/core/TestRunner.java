b859260a303fa014c160df90dd42a6e17ca146e7
Jay <beginin15@gmail.com>
2020-08-20 23:00:43 +0900

    [abstraction] 등록된 Converter와 Formatter 출력해보기
    - 스프링 부트에서 기본으로 주입해주는 ConversionService는 WebConversionService

---
+++ src/main/java/com/course/spring_framework/core/TestRunner.java
@@ -0,0 +1,20 @@
+package com.course.spring_framework.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements ApplicationRunner {
+
+    @Autowired
+    ConversionService conversionService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(conversionService);
+        System.out.println(conversionService.getClass());
+    }
+}
