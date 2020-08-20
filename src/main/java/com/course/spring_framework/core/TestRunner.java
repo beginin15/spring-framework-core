82aa3086a367dc91105c94df437f1ff941a80272
Jay <beginin15@gmail.com>
2020-08-20 17:37:16 +0900

    [resource] prefix를 붙이지 않으면 웹 애플리케이션 루트에서 리소스를 찾는 것 확인
    - prefix를 붙이지 않았기 때문에 서블릿 기반의 리소스 타입이어야 하는데 예상과 다르게 클래스 패스 기반 리소스였다. (결과가 false여야 하는데 true 리턴)
    
    - 원인을 살펴보니, 일반 스프링 부트가 아닌 spring-boot-starter-web 을 이용하여 스프링 MVC 프로젝트일 때, ApplicationContext가 기본으로 WebApplicationContext 계열을 주입해주는 것이었다. 스프링 MVC 라이브러리로 변경해주었다.
    
    - 스프링 MVC일 때 AnnotaionConfigServletWebServerApplicationContext, 아닐 때 AnnotaionConfigApplicationContext가 주입된다.

--- src/main/java/com/course/spring_framework/core/TestRunner.java
+++ src/main/java/com/course/spring_framework/core/TestRunner.java
@@ -3,22 +3,23 @@ package com.course.spring_framework.core;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TestRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println(resourceLoader.getClass());
 
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        Resource resource = resourceLoader.getResource("test.txt");
         System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
     }
