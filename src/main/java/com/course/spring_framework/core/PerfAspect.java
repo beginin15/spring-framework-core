a7604bae1ee8c1a53d73d1d3f33d957aedc162fe
Jay <beginin15@gmail.com>
2020-09-10 23:26:58 +0900

    [AOP] 스프링 AOP를 이용하여 Aspect 등록하는 방법

---
+++ src/main/java/com/course/spring_framework/core/PerfAspect.java
@@ -0,0 +1,19 @@
+package com.course.spring_framework.core;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerfAspect {
+
+    @Around("execution(* com.course..*.EventService.*(..))")
+    public Object logPerf(ProceedingJoinPoint pjp) throws Throwable {
+        long begin = System.currentTimeMillis();
+        Object retVal = pjp.proceed();
+        System.out.println(System.currentTimeMillis() - begin);
+        return retVal;
+    }
+}
