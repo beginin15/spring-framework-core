662b13b0cfd19e611e5ceade30738bc2d417b534
Jay <beginin15@gmail.com>
2020-09-10 22:34:09 +0900

    [etc] 웹 서버를 기동시키지 않도록 설정 변경. 일반 자바 프로그램처럼 실행하는 방법

--- src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
+++ src/main/java/com/course/spring_framework/core/SpringFrameworkCoreApplication.java
@@ -1,13 +1,16 @@
 package com.course.spring_framework.core;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringFrameworkCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringFrameworkCoreApplication.class, args);
+        SpringApplication app = new SpringApplication(SpringFrameworkCoreApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
 }
