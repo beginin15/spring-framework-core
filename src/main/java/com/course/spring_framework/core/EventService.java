bced3b1b88d0184c94b7d7c3ebff95fd64e2e168
Jay <beginin15@gmail.com>
2020-09-11 00:12:22 +0900

    [Null-Safety] 툴의 지원을 받아 컴파일 타임에 NullPointerException을 예방하는 어노테이션

---
+++ src/main/java/com/course/spring_framework/core/EventService.java
@@ -0,0 +1,13 @@
+package com.course.spring_framework.core;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    @NonNull
+    public String createEvent(@NonNull String name) {
+        return null;
+    }
+}
---
+++ src/main/java/com/course/spring_framework/core/TestRunner.java
@@ -0,0 +1,18 @@
+package com.course.spring_framework.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements ApplicationRunner {
+
+    @Autowired
+    EventService eventService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        String message = eventService.createEvent(null);
+    }
+}
