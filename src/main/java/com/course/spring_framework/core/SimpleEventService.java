72bf3a8fd38366077fab500daadee2e3127b4fad
Jay <beginin15@gmail.com>
2020-09-10 22:31:06 +0900

    [AOP] 프록시 패턴을 이용하여 흩어진 관심사를 비즈니스 로직으로부터 분리

--- src/main/java/com/course/spring_framework/core/SimpleEventService.java
+++ src/main/java/com/course/spring_framework/core/SimpleEventService.java
@@ -7,30 +7,22 @@ public class SimpleEventService implements EventService{
 
     @Override
     public void createEvent() {
-        long begin = System.currentTimeMillis(); // 성능 측정 기능 (흩어진 관심사)
-
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("Create an event");
-
-        System.out.println(System.currentTimeMillis() - begin); // 성능 측정 기능 (흩어진 관심사)
     }
 
     @Override
     public void publicEvent() {
-        long begin = System.currentTimeMillis(); // 성능 측정 기능 (흩어진 관심사)
-        
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("Public an event");
-
-        System.out.println(System.currentTimeMillis() - begin); // 성능 측정 기능 (흩어진 관심사)
     }
 
     @Override
