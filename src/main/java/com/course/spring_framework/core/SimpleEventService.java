3a58bbe2924e669f3dfdbfda018a77e0966a5557
Jay <beginin15@gmail.com>
2020-09-10 22:17:04 +0900

    [AOP] 부가적인 기능을 수행하는 흩어진 관심사

--- src/main/java/com/course/spring_framework/core/SimpleEventService.java
+++ src/main/java/com/course/spring_framework/core/SimpleEventService.java
@@ -7,22 +7,30 @@ public class SimpleEventService implements EventService{
 
     @Override
     public void createEvent() {
+        long begin = System.currentTimeMillis(); // 성능 측정 기능 (흩어진 관심사)
+
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("Create an event");
+
+        System.out.println(System.currentTimeMillis() - begin); // 성능 측정 기능 (흩어진 관심사)
     }
 
     @Override
     public void publicEvent() {
+        long begin = System.currentTimeMillis(); // 성능 측정 기능 (흩어진 관심사)
+        
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("Public an event");
+
+        System.out.println(System.currentTimeMillis() - begin); // 성능 측정 기능 (흩어진 관심사)
     }
 
     @Override
